62838b71a77ffc45d5f6cf5ca3229860121388b8
Paul Gustafson <paul.gustafson@gmail.com>
2020-08-30 13:34:28 -0400

    Split eval + let; docstrings

--- lembot.py
+++ lembot.py
@@ -9,10 +9,15 @@ import shutil
 
 @module.commands('printall')
 def printall(bot, trigger):
+    """
+    Prints all definitions of a given function name. The pinned definition (if
+    it exists) will be annotated with an asterisk.
+
+    """
     functionName = trigger.group(2).split()[0]
 
     pin = -1
-    with SqliteDict(filename='lembrary/ws_' + trigger.nick + '.sqlite') as pinDict:
+    with SqliteDict(filename='lembrary/pins/' + trigger.nick + '.sqlite') as pinDict:
         if functionName in pinDict:
             pin = pinDict[functionName]
             
@@ -26,22 +31,27 @@ def printall(bot, trigger):
                 lines = f.read().splitlines()
 
                 if i == pin:
-                    bot.reply("(Pinned) " + functionName + " " + i " : ")
+                    bot.reply(functionName + " " + i "*: ")
                 else:
                     bot.reply(functionName + " " + i ": " )
 
                 
                 for l in lines:
-                    bot.reply( l)
+                    bot.reply(l)
 
                 bot.reply("")
 
 @module.commands('print')
 def print(bot, trigger):
+    """ 
+    Prints the currently active definition of a function name.  This is
+    the pinned definition if it exists.  Otherwise, it is the last-defined
+    definition.
+    """
     functionName = trigger.group(2).split()[0]
     
     pin = -1
-    with SqliteDict(filename='lembrary/ws_' + trigger.nick + '.sqlite') as pinDict:
+    with SqliteDict(filename='lembrary/pins/' + trigger.nick + '.sqlite') as pinDict:
         if functionName in pinDict:
             pin = pinDict[functionName]
             
@@ -60,13 +70,16 @@ def print(bot, trigger):
                         
 @module.commands('pin')
 def pin(bot, trigger):
+    """
+    Pins a name to a specified definition.  
+    """
     tokens = trigger.group(2).split()
     functionName = tokens[0]
 
 
     with SqliteDict(filename='lembrary/fn_mod_dict.sqlite') as fmDict:
 
-        with SqliteDict(filename='lembrary/ws_' + trigger.nick + '.sqlite') as pinDict:
+        with SqliteDict(filename='lembrary/pins/' + trigger.nick + '.sqlite') as pinDict:
             if not functionName in fmDict:
                 bot.reply(trigger.group(2) + " not found.")
                 return
@@ -87,64 +100,122 @@ def pin(bot, trigger):
 
             
 @module.commands('pins')
-def pins(bot, trigger): 
-    with SqliteDict(filename='lembrary/ws_' + trigger.nick + '.sqlite') as pinDict:
+def pins(bot, trigger):
+    """
+    Prints all of your currently active pins.
+    """
+    with SqliteDict(filename='lembrary/pins/' + trigger.nick + '.sqlite') as pinDict:
         ans = "Pins: "
         for k in pinDict.keys():
             ans += "(" + k + " " + str(pinDict[k]) + ") "
         bot.reply(ans)
 
-@module.commands('new_workspace')
-def newpins(bot, trigger):
-    saveWorkspace(bot, trigger)
-    os.remove('lembrary/ws_' + trigger.nick + '.sqlite')
+@module.commands('clear_pins')
+def new_pins(bot, trigger):
+    """
+    Clears all pins after saving a backup.
+    """
+    save_pins(bot, trigger)
+    os.remove('lembrary/pins/' + trigger.nick + '.sqlite')
     bot.reply('Workspace cleared.')
    
 
-@module.commands('save_workspace')
-def saveWorkspace(bot,trigger):
+@module.commands('save_pins')
+def save_pins(bot,trigger):
+    """
+    Saves your current pins.
+    """
     dest = trigger.nick + "_" + str(int(1000*time.time()))
-    shutil.copy("lembrary/ws_" + trigger.nick + ".sqlite",
-                "lembrary/savedWs_" + dest + ".sqlite")
+    shutil.copy("lembrary/pins/" + trigger.nick + ".sqlite",
+                "lembrary/savedPins/" + dest + ".sqlite")
     bot.reply("Saved workspace: " + dest)
         
     
-@module.commands('load_workspace')
+@module.commands('load_pins')
 def loadWorkspace(bot, trigger):
+    """
+    Load previously saved pins.
+    """
     dest = trigger.group(2)
-    shutil.copy("lembrary/savedWs_" + dest + ".sqlite",
-                "lembrary/ws_" + trigger.nick + ".sqlite")
+    shutil.copy("lembrary/savedPins/" + dest + ".sqlite",
+                "lembrary/pins/" + trigger.nick + ".sqlite")
     bot.reply("Loaded workspace: " + dest)
             
         
-        
-# TODO: mutual recursion?    
-@module.commands('eval', 'let')
+    
+@module.commands('eval')
 def eval(bot, trigger):
+    """
+    Evaluate an expression.
+    """
     expr = trigger.group(2)        
     
-    if trigger.group(1) == 'eval':
-        tokens = re.split('\W+', expr)
-    else:
-        eqSign = expr.index('=')
-        tokens = re.split('\W+', expr[eqSign:])
-        
+
+    tokens = re.split('\W+', expr)
+            
     imports = []
     
     with SqliteDict(filename='lembrary/fn_mod_dict.sqlite') as fmDict:
         for t in tokens:
             if t in fmDict:
-                with SqliteDict(filename='lembrary/ws_' + trigger.nick + '.sqlite') as pinDict:
+                with SqliteDict(filename='lembrary/pins/' + trigger.nick + '.sqlite') as pinDict:
                     if t in pinDict:
                         imports.append(fmDict[t][pinDict[t]])
                     else:
                         imports.append(fmDict[t][-1])
 
 
-    if trigger.group(1) == 'eval':
-        moduleName = "Eval_" + trigger.nick + "_" + str(int(1000*time.time()))
-    else:
-        moduleName = "Fun_" + trigger.nick + "_" +  str(int(1000*time.time()))
+    moduleName = "Eval_" + trigger.nick + "_" + str(int(1000*time.time()))
+    
+    if re.search(r'\W', moduleName) != None:
+        bot.reply('Illegal nick: only alphanumerics and underscores allowed')
+        return
+
+    contents = "module " + moduleName + " where \n" 
+    for i in imports:
+        contents += "import " + i + "\n"
+
+    contents += "main = print $ " + expr + "\n"
+    
+        
+    path = 'lembrary/' + moduleName + '.hs'    
+    with open(path, "w+") as f:
+        print("FILE CREATED: " + path)
+        f.write(contents)
+
+        
+    cmd = 'runghc'
+            
+    result = subprocess.run([cmd, '-ilembrary',  path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    lines = result.stdout.decode('UTF-8').splitlines()
+    ans = '   '.join(lines)
+    bot.reply(ans)
+    
+
+@module.commands('let')
+def let(bot, trigger):
+    """
+    Define a function.
+    """
+    expr = trigger.group(2)        
+    
+    eqSign = expr.index('=')
+    tokens = re.split('\W+', expr[eqSign:])
+        
+    imports = []
+    
+    with SqliteDict(filename='lembrary/fn_mod_dict.sqlite') as fmDict:
+        for t in tokens:
+            if t in fmDict:
+                with SqliteDict(filename='lembrary/pins/' + trigger.nick + '.sqlite') as pinDict:
+                    if t in pinDict:
+                        imports.append(fmDict[t][pinDict[t]])
+                    else:
+                        imports.append(fmDict[t][-1])
+
+
+    
+    moduleName = "Def_" + fun + "_" +  str(int(1000*time.time()))
 
     if re.search(r'\W', moduleName) != None:
         bot.reply('Illegal nick: only alphanumerics and underscores allowed')
@@ -171,20 +242,21 @@ def eval(bot, trigger):
         
     result = subprocess.run([cmd, '-ilembrary',  path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     lines = result.stdout.decode('UTF-8').splitlines()
-    ans = '   '.join(lines)
-    bot.reply(ans)
-    
 
-    if trigger.group(1) == 'let':
-        functionName = expr.split()[0]
+    if len(lines) == 1:
+        functionName = expr[:eqSign].split()[0]
         with SqliteDict(filename='lembrary/fn_mod_dict.sqlite') as fmDict:
             if not functionName in fmDict:
                 fmDict[functionName] = []
-            modList = fmDict[functionName]
-            modList.append(moduleName)
-            fmDict[functionName] = modList
-            fmDict.commit()
+                modList = fmDict[functionName]
+                modList.append(moduleName)
+                fmDict[functionName] = modList
+                fmDict.commit()
 
+    else:
+        ans = '   '.join(lines)
+        bot.reply(ans)
+    
 
     
                 
